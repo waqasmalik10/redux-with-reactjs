baac271fd2cff5039375addf72efa6718b01170b
waqasmalik1010 <waqasmalikbsse@gmail.com>
2019-12-26 12:00:26 +0500

    Handling ASynchronous code

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -31,10 +31,20 @@ export const subtract = (value) => {
     }
 }
 
-export const storeResult = (res) => {
+export const saveResult = (res) => {
     return {
         type: STORE_RESULT,
         counter: res
+    }
+}
+
+export const storeResult = (res) => {
+    return (dispatch) => {
+        setTimeout( () => {
+                dispatch(saveResult(res))
+            },
+            3000
+        )
     }
 }
 
